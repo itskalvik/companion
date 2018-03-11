c552a32a76ba61e2b7316374024997d822ee5bac
kalvik <kd.kalvik@gmail.com>
2018-03-10 22:27:15 -0600

    updated mavproxy parameters

--- start_mavproxy.sh
+++ start_mavproxy.sh
@@ -10,4 +10,12 @@ set -x
 /home/nvidia/.local/bin/mavproxy.py \
     --master=/dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00,115200 \
     --load-module='GPSInput,DepthOutput' \
-    --out=udpin:0.0.0.0:14550
+    --out udpbcast:192.168.1.255:14550 \
+    --out udpin:localhost:9000 \
+    --source-system=200 \
+    --cmd="set heartbeat 0"
+
+
+
+
+
