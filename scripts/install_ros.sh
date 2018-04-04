77596615d3985325a2ecbc43256f538ad287a2b5
kalvik jakkala <kd.kalvik@gmail.com>
2018-04-04 16:40:51 -0500

    Updated script to do sanity checks and added script to install ros

---
+++ scripts/install_ros.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [[ $UID != 0 ]]; then
+	echo "This script require root privileges!" 1>&2
+	exit 1
+fi
+
+#setup sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+#set up keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+#update
+sudo apt-get update
+
+#install ros base
+sudo apt-get install ros-kinetic-ros-base
+
+#initialize rosdep
+sudo rosdep init
+rosdep update
+
+#setup environment
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#install dependencies
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+#setup catkin workspace
+cd $HOME
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+
+#setup environment
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#install mavros
+sudo apt-get install ros-kinetic-mavros ros-kinetic-mavros-extras ros-kinetic-mavros-msgs
+
+#install libgeographic
+cd $HOME
+
+sudo apt-get install libgeographic-dev
+sudo apt-get install geographiclib-tools
+
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+
+chmod a+x install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,26 @@
-#!/bin/bash''
+#!/bin/bash
 
-cd /home/nvidia
+#Make sure scriot was run with root user privileges
+if [[ $UID != 0 ]]; then
+	echo "This script require root privileges!" 1>&2
+	exit 1
+fi
+
+#Function to clone repo if it doesnt exist and update repo if it exists
+get_repo(){
+	if [ -d $1 ]
+	then
+		cd $1
+		git reset --hard origin/master
+		git pull origin master
+		cd ..
+	else
+		git clone $2 $1
+	fi
+}
+
+#get companion repo
+get_repo "$HOME/companion" "https://github.com/kdkalvik/companion.git"
 
 sudo apt-get purge libreoffice-*
 
@@ -11,10 +31,11 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository universe -y
 sudo apt-get update
 
-sudo apt-get install git -y
+sudo apt-get install git screen -y
 sudo apt-get install -y python-dev python-opencv python-pip python-libxml2  python-wxgtk3.0 python-matplotlib python-pygame
 sudo apt-get install -y python-setuptools python-dev build-essential
 sudo apt-get install -y libxml2-dev libxslt1-dev
+sudo apt-get install gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly
 sudo pip install pip -U
 sudo pip install future
 sudo pip install pyserial -U
@@ -25,8 +46,7 @@ sudo adduser $USER dialout
 echo "export PATH=$PATH:$HOME/.local/bin" >> ~/.bashrc
 source ~/.bashrc
 
-git clone https://github.com/mavlink/mavlink.git /home/nvidia/mavlink
-
+get_repo "$HOME/mavlink" "https://github.com/mavlink/mavlink.git"
 pushd mavlink
 git submodule init && git submodule update --recursive
 pushd pymavlink
@@ -34,13 +54,10 @@ sudo python setup.py build install
 popd
 popd
 
-git clone https://github.com/bluerobotics/MAVProxy.git /home/nvidia/mavproxy
-
+get_repo "$HOME/mavproxy" "https://github.com/ArduPilot/MAVProxy.git"
 pushd mavproxy
 sudo python setup.py build install
 popd
-
-sudo apt-get install -y screen
 
 sudo sed -i -e '$i \sleep 10\n' /etc/rc.local
 sudo sed -i -e '$i \sudo -H -u nvidia /bin/bash -c '/home/nvidia/companion/scripts/autostart_mavproxy.sh'\n' /etc/rc.local
@@ -49,7 +66,7 @@ sudo sed -i -e '$i \sudo -H -u nvidia /bin/bash -c '/home/nvidia/companion/scrip
 sudo sh -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"26ac\", ATTRS{idProduct}==\"0011\", SYMLINK+=\"pixhawk\"' > /etc/udev/rules.d/99-usb-serial.rules"
 sudo udevadm trigger
 
-mkdir $HOME/companion/logs
+sudo ./install_ros.sh
 
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
